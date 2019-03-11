c544bd155dc2a8e98689d7f9c759c410ed92ac78
mhd <914685196@qq.com>
2019-03-11 18:04:21 +0800

    【3月11日下班了老弟】

---
+++ api/modules/v1.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace api\modules;
+
+/**
+ * modules module definition class
+ */
+class v1 extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'api\modules\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
